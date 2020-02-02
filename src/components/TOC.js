218985d98a8e7cf27678b6387a63cc14fd644fb1
soompy <sooper1137@gmail.com>
2020-02-03 00:20:36 +0900

    컴포넌트 이벤트 만들기

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -8,7 +8,26 @@ class TOC extends Component {
       var data = this.props.data;
       var i = 0;
       while(i < data.length){
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);      
+        lists.push(
+          <li key={data[i].id}>
+            <a 
+              href={"/content/"+data[i].id}
+              data-id = {data[i].id}
+              
+              /*
+                // 속성을 이용한 방식
+                onClick={function(e){
+                  e.preventDefault();
+                  this.props.onChangePage(e.target.dataset.id);
+                }.bind(this)}
+              */
+
+              onClick={function(id, e){                
+                e.preventDefault();
+                this.props.onChangePage(id);
+              }.bind(this, data[i].id)}
+            >{data[i].title}</a>
+          </li>);      
         i = i + 1;
       }
       return (
